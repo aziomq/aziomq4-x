6d9e686f929b4cbeecbc08023b8f1ec9c9b12ace
Thomas Rodgers <trodgers@drw.com>
2014-03-31 09:07:26 -0500

    Cleaning up mixed socket example code

--- doc/examples/mixed_sockets/main.cpp
+++ doc/examples/mixed_sockets/main.cpp
@@ -9,21 +9,45 @@
 
 namespace asio = boost::asio;
 
-void setup_socket(asio::ip::udp::socket & socket,
-                  const asio::ip::udp::endpoint & group,
-                  const asio::ip::address_v4 & listen) {
-    socket.open(group.protocol());
-    socket.set_option(asio::ip::udp::socket::reuse_address(true));
-    socket.bind(asio::ip::udp::endpoint(listen, group.port()));
-    socket.set_option(asio::ip::multicast::join_group(listen, group.address().to_v4()));
-}
 
-size_t translate(const char* exch_buf, char* res_buf, size_t packet_size) {
-    // normalize external representation
-    // ...
-    std::copy_n(exch_buf, packet_size, res_buf);
-    return packet_size;
-}
+struct translator {
+    asio::ip::udp::socket & frontend;
+    aziomq::socket & backend;
+    translator(asio::ip::udp::socket & f,
+               aziomq::socket & b,
+               const asio::ip::udp::endpoint & group,
+               const std::string & topic_uri,
+               const asio::ip::address_v4 & listen = asio::ip::address_v4()) :
+        frontend(f), backend(b) {
+            frontend.open(group.protocol());
+            frontend.set_option(asio::ip::udp::socket::reuse_address(true));
+            frontend.bind(asio::ip::udp::endpoint(listen, group.port()));
+            frontend.set_option(asio::ip::multicast::join_group(listen, group.address().to_v4()));
+
+            backend.bind(topic_uri);
+        }
+
+    void operator()(const boost::system::error_code & ec = boost::system::error_code(),
+                    size_t bytes_transferred = 0) {
+            if (ec)
+                return;
+            if (bytes_transferred) {
+                auto bufsize = translate(exch_buf.data(), pub_buf.data(), bytes_transferred);
+                backend.send(asio::buffer(const_cast<const char*>(pub_buf.data()), bufsize));
+            }
+            frontend.async_receive(asio::buffer(exch_buf), *this);
+    }
+
+private:
+    std::array<char, 4096> exch_buf;
+    std::array<char, 4096> pub_buf;
+    size_t translate(const char* exch_buf, char* res_buf, size_t packet_size) {
+        // normalize external representation
+        // ...
+        std::copy_n(exch_buf, packet_size, res_buf);
+        return packet_size;
+    }
+};
 
 int main(int argc, char **argv) {
     asio::io_service ios;
@@ -31,30 +55,19 @@ int main(int argc, char **argv) {
     // to change context options you must do so before any sockets are created
     aziomq::io_service::set_option(ios, aziomq::io_service::io_threads(2));
 
-    // receive exchange traffic on udp multicast
-    asio::ip::udp::socket exchange(ios);
-    asio::ip::udp::endpoint group;
-    group.address(asio::ip::address::from_string("242.1.1.1"));
-    group.port(42424);
-
-    setup_socket(exchange, group, asio::ip::address_v4());
-
-    aziomq::socket publisher(ios, ZMQ_PUB);
-    publisher.bind("ipc://nasdaq-feed");
-
-    std::array<char, 4096> exch_buf;
-    std::array<char, 4096> pub_buf;
-    exchange.async_receive(asio::buffer(exch_buf), [&](const boost::system::error_code & ec,
-                                                       size_t bytes_transferred) {
-            if (ec)
-                return;
-            auto bufsize = translate(exch_buf.data(), pub_buf.data(), bytes_transferred);
-            publisher.send(asio::buffer(const_cast<const char*>(pub_buf.data()), bufsize));
-    });
-
     // run until sigterm or sigint
     asio::signal_set signals(ios, SIGINT, SIGTERM);
     signals.async_wait([&](const boost::system::error_code &, int) { ios.stop(); });
+
+    asio::ip::udp::endpoint group;
+    group.address(asio::ip::address::from_string("242.1.1.1"));
+    group.port(42424);
+    asio::ip::udp::socket listen(ios);
+    aziomq::socket publish(ios, ZMQ_PUB);
+
+    // register the translator on listen/publish sockets
+    translator(listen, publish, group, "ipc://nasdaq-feed")();
+
     // spawn threads to handle inproc messaging
     // ...
     ios.run();
